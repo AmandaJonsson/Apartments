578523c0494ce136b5e0fdfa80e36e53d4d04401
Emilia Vestlund <emiliavestlund@hotmail.com>
2018-02-23 13:15:23 +0100

    entitymanager doesn't work in master

--- apartments/src/main/java/Rest/ApplicationConfig.java
+++ apartments/src/main/java/Rest/ApplicationConfig.java
@@ -25,6 +25,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(Rest.CORSFilter.class);
         //resources.add(Apartments.Rest.AuthorResource.class);
         //resources.add(apartments.Rest.CORSFilter.class);
         //resources.add(chl.hajo.library.rest.DatabaseResource.class);
