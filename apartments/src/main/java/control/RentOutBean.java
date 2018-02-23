38d123fc36fb0f60d0517fffbb51d4c567837815
Therese Sturesson <thestu@student.chalmers.se>
2018-02-23 11:34:38 +0100

    added som methods

--- apartments/src/main/java/control/RentOutBean.java
+++ apartments/src/main/java/control/RentOutBean.java
@@ -5,10 +5,35 @@
  */
 package control;
 
+import core.RentOut;
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
 /**
  * @author tthera
  */
 
-public class RentOutBean {
+@Named("rentOut")
+@SessionScoped
+public class RentOutBean implements Serializable {
+    
+    
+    
+    
+    public void setRentOut () {}
+    
+    
+    public List<RentOut> findAll() {
+        return null;
+    }
+    
+    
+    public void add () {}
+    
+    public void update () {}
+    
+    public void delete () {}
     
 }
--- apartments/src/main/java/dao/RentOutCollection.java
+++ apartments/src/main/java/dao/RentOutCollection.java
@@ -5,10 +5,52 @@
  */
 package dao;
 
+import core.RentOut;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
- *
  * @author tthera
  */
-public class RentOutCollection {
+
+@Stateless
+public class RentOutCollection extends AbstractQuery<RentOut, String>{
+    
+    
+    @PersistenceContext(unitName ="")       //Vad ska den heta?
+    private EntityManager em;
+    
+    
+    public RentOutCollection() {
+        super(RentOut.class, QRentOut.rentOut);
+    }
+    
+    
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+    
+    
+    @Override
+    public void setEntityManager(EntityManager em) {
+        this.em = em;
+    }
+    
+    //Oklart om den ska vara såhär? Används den?
+    /*public List<RentOut> findByName(String personName) {
+        QRentOut rentOut = QRentOut.rentOut;
+        JPAQueryFactory qf = new JPAQueryFactory(em);
+        List<RentOut> foundName = qf.select(rentOut)
+                .from(rentOut)
+                .where(rentOut.name.eq(personName))
+                .fetch();
+        System.out.print(foundName);
+        return foundName;
+    
+    }*/
+    
     
 }
