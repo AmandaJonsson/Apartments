5512a8b9799e36c4306db789d84c372b66cbf7e0
Therese Sturesson <thestu@student.chalmers.se>
2018-03-02 13:49:37 +0100

    changed the frontend

--- frontend/control/RentOutCtrl.js
+++ frontend/control/RentOutCtrl.js
@@ -1,1 +1,63 @@
 
+
+import {
+  reOuReg
+} from "../model/RentOutRegistry.js"
+import {
+  RentOut
+} from "../model/RentOut.js"
+import {
+  eventBus as eB
+} from "../util/eventBus.js"
+
+
+class Listener {
+    
+    
+    onModelEvent(event, data) {
+        var table = $('#rentOut').DataTable();
+        table.rows().remove();
+        table.rows.add(data).draw();
+    }
+    
+    
+    update() {
+        //console.log("update");
+        let id = $("#rid").val();
+        let adr = $("#radress").val();
+        let des = $("#rdescription").val();
+        let name = $("#rname").val();
+        let pho = $("#rphoneNr").val();
+        let mail = $("#rmail").val();
+        let img = $("#rimage").val();
+        let reOu = new RentOut(id, adr, des, name, pho, mail, img);
+        reOuReg.update(reOu);
+    }
+    
+    
+    delete() {
+        //console.log("delete");
+        let id = $("#rid").val();
+        reOuReg.delete(id);
+    }
+    
+    
+    create() {
+        //console.log("create");
+        let id = $("#rid").val();
+        let adr = $("#radress").val();
+        let des = $("#rdescription").val();
+        let name = $("#rname").val();
+        let pho = $("#rphoneNr").val();
+        let mail = $("#rmail").val();
+        let img = $("#rimage").val();
+        let reOu = new RentOut(id, adr, des, name, pho, mail, img);
+        reOuReg.create(reOu);
+    }
+    
+}
+
+const listener = new Listener();
+eB.register(listener);
+
+
---
+++ frontend/model/RentOutRegistry.js
@@ -0,0 +1,60 @@
+
+import {
+  RentOut
+} from "./RentOut.js"
+import {
+  eventBus as eB
+} from "../util/eventBus.js"
+import {
+  RentOutService
+} from "../service/RentOutService.js"
+
+
+class RentOutRegistry {
+   
+   
+   constructor() {
+       this.retOuts = [];
+    } 
+
+
+    find(id) {
+        return this.rentOuts.find(a => a.id === id);
+    }
+    
+    
+    findAll() {
+        RentOutService.findAll(data => {
+          this.rentOuts = data;
+          return eB.notify("", data);
+        });
+    }
+    
+    
+    create(rentOut) {
+        RentOutService.create(rentOut, data => {
+            return eB.notify("ADD", data);
+        }); 
+    }
+    
+    
+    update(rentOut) {
+        RentOutService.update(rentOut, callback => {
+            var a = this.find(rentOut.id);
+            this.rentOuts = callback;
+            eB.notify("UPDATE", this.rentOuts);
+        });
+    }
+    
+    
+    delete(id) {
+        RentOutService.delete(id, data => {
+           var a = this.find(id);
+           this.rentOuts = this.rentOuts.filter(elem => elem !== a);
+           return eB.notify("DELETE", this.rentOuts); 
+        });
+    }
+    
+}
+
+export const reOuReg = new RentOutRegistry();
